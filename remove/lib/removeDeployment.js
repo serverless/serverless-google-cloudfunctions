3dd6e2e5304c77ea6d3d96779b48053887e4dfb6
Philipp Muens <raute1337@gmx.de>
2017-02-28 10:20:34 +0100

    Add remove plugin

---
+++ remove/lib/removeDeployment.js
@@ -0,0 +1,15 @@
+module.exports = {
+  removeDeployment() {
+    this.serverless.cli.log('Removing deployment...');
+
+    const deploymentName = `sls-${this.serverless.service.service}-${this.options.stage}`;
+
+    const params = {
+      project: this.serverless.service.provider.project,
+      deployment: deploymentName,
+    };
+
+    return this.provider.request('deploymentmanager', 'deployments', 'delete', params)
+      .then(() => this.monitorDeployment(deploymentName, 'remove', 5000));
+  },
+};
