1dae6aea7fba864c4d680c6bd1097bb18fa515ed
Philipp Muens <raute1337@gmx.de>
2017-02-28 10:15:21 +0100

    Add shared functionality

---
+++ shared/utils.js
@@ -0,0 +1,12 @@
+const BbPromise = require('bluebird');
+
+module.exports = {
+  setDefaults() {
+    this.options.stage = this.options.stage
+      || 'dev';
+    this.options.region = this.options.region
+      || 'us-central1';
+
+    return BbPromise.resolve();
+  },
+};
---
+++ shared/utils.test.js
@@ -0,0 +1,34 @@
+const setDefaults = require('./utils');
+const GoogleProvider = require('../provider/googleProvider');
+const Serverless = require('../test/serverless');
+const GoogleCommand = require('../test/googleCommand');
+
+describe('Utils', () => {
+  let serverless;
+  let googleCommand;
+
+  beforeEach(() => {
+    serverless = new Serverless();
+    serverless.setProvider('google', new GoogleProvider(serverless));
+    googleCommand = new GoogleCommand(serverless, {}, setDefaults);
+  });
+
+  describe('#setDefaults()', () => {
+    it('should set default values for options if not provided', () => googleCommand
+      .setDefaults().then(() => {
+        expect(googleCommand.options.stage).toEqual('dev');
+        expect(googleCommand.options.region).toEqual('us-central1');
+      }),
+    );
+
+    it('should set the options when they are provided', () => {
+      googleCommand.options.stage = 'my-stage';
+      googleCommand.options.region = 'my-region';
+
+      return googleCommand.setDefaults().then(() => {
+        expect(googleCommand.options.stage).toEqual('my-stage');
+        expect(googleCommand.options.region).toEqual('my-region');
+      });
+    });
+  });
+});
