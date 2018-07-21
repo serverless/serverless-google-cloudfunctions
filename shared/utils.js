b6d96684ff6a45533a676dd58fc41b210b4c02ac
Toan Nguyen <toannp90@gmail.com>
2018-07-21 12:02:27 +0700

    temporary fix for multiple regions

--- shared/utils.js
+++ shared/utils.js
@@ -7,9 +7,14 @@ module.exports = {
   setDefaults() {
     this.options.stage = _.get(this, 'options.stage')
       || 'dev';
-    this.options.region = _.get(this, 'options.region')
-      || _.get(this, 'serverless.service.provider.region')
-      || 'us-central1';
+    
+    // serverless framework is hard-coding us-east-1 region from aws 
+    // this is temporary fix for multiple regions
+    const region = _.get(this, 'options.region')
+      || _.get(this, 'serverless.service.provider.region');
+
+    this.options.region = (!region || region === 'us-east-1') 
+      ? 'us-central1' : region;
 
     return BbPromise.resolve();
   },
