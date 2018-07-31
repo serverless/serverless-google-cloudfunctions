11b985044c18eab3ba1915b3075a1944edb477f2
Kenneth Falck <kennu@iki.fi>
2018-07-31 18:30:59 +0300

    Allow setting provider.region and provider.stage in serverless.yml

--- shared/utils.js
+++ shared/utils.js
@@ -6,8 +6,10 @@ const BbPromise = require('bluebird');
 module.exports = {
   setDefaults() {
     this.options.stage = _.get(this, 'options.stage')
+      || _.get(this, 'serverless.service.provider.stage')
       || 'dev';
     this.options.region = _.get(this, 'options.region')
+      || _.get(this, 'serverless.service.provider.region')
       || 'us-central1';
 
     return BbPromise.resolve();
