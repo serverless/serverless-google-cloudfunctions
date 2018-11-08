3857c1aa40a09d9bd069580cff478b5cb70034ab
Philipp Muens <raute1337@gmx.de>
2018-11-08 14:33:22 +0100

    Fix default region setting

--- shared/utils.js
+++ shared/utils.js
@@ -5,18 +5,19 @@ const BbPromise = require('bluebird');
 
 module.exports = {
   setDefaults() {
-    this.options.stage = _.get(this, 'options.stage')
-      || 'dev';
-    this.options.runtime = _.get(this, 'options.runtime')
-      || 'nodejs8';
+    this.options.stage = _.get(this, 'options.stage') || 'dev';
+    this.options.runtime = _.get(this, 'options.runtime') || 'nodejs8';
 
     // serverless framework is hard-coding us-east-1 region from aws
     // this is temporary fix for multiple regions
-    const region = _.get(this, 'options.region')
-      || _.get(this, 'serverless.service.provider.region');
+    let region = _.get(this, 'options.region') || _.get(this, 'serverless.service.provider.region');
 
-    this.options.region = (!region || region === 'us-east-1')
-      ? 'us-central1' : region;
+    if (region === 'us-east-1') {
+      region = 'us-central1';
+    }
+
+    this.options.region = region;
+    this.serverless.service.provider.region = region;
 
     return BbPromise.resolve();
   },
