1057f706ad451cef8273669639d9e584639b1c9f
markrity <11563691+markrity@users.noreply.github.com>
2022-01-24 18:48:19 +0200

    Add support for nodejs16 runtime

--- provider/googleProvider.js
+++ provider/googleProvider.js
@@ -61,6 +61,7 @@ class GoogleProvider {
             'nodejs10',
             'nodejs12',
             'nodejs14',
+            'nodejs16', // recommended
             'python37',
             'python38',
             'python39',
